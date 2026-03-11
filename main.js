8237a86f8890b66e0651468ba398d0835d558b6e
sanghyeonkim0216 <sanghyeonkim0216@gmail.com>
2026-03-11 14:01:31 +0000

    Add initial project files

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-// Add JS here
+class LottoBall extends HTMLElement {
+    constructor() {
+        super();
+        const shadow = this.attachShadow({ mode: 'open' });
+        const number = this.getAttribute('number');
+        const ball = document.createElement('div');
+        ball.textContent = number;
+        shadow.appendChild(ball);
+    }
+}
+
+customElements.define('lotto-ball', LottoBall);
+
+document.getElementById('generate-btn').addEventListener('click', () => {
+    const lottoBallsContainer = document.getElementById('lotto-balls-container');
+    lottoBallsContainer.innerHTML = '';
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        numbers.add(Math.floor(Math.random() * 45) + 1);
+    }
+
+    numbers.forEach((number, index) => {
+        setTimeout(() => {
+            const lottoBall = document.createElement('lotto-ball');
+            lottoBall.setAttribute('number', number);
+            lottoBallsContainer.appendChild(lottoBall);
+        }, index * 300);
+    });
+});
